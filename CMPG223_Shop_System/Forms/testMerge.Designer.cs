0414a194a794f0ddd770e2ce0d5fb56368284d70
kayleeyana <najmahabrahaham@gmail.com>
2022-09-08 16:16:07 +0200

    added another test button

--- CMPG223_Shop_System/Forms/testMerge.Designer.cs
+++ CMPG223_Shop_System/Forms/testMerge.Designer.cs
@@ -34,6 +34,7 @@ namespace CMPG223_Shop_System.Forms
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -80,11 +81,21 @@ namespace CMPG223_Shop_System.Forms
             this.textBox2.Size = new System.Drawing.Size(100, 26);
             this.textBox2.TabIndex = 4;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // testMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 563);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -105,5 +116,6 @@ namespace CMPG223_Shop_System.Forms
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
